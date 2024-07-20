fcb7da67cf2e7ec6601afceb93aebc0465f83c41
soysauce2393 <hubblesoren@gmail.com>
2024-07-20 13:13:00 -0700

    intake assist things

--- src/main/java/frc/robot/commands/IntakeAssist.java
+++ src/main/java/frc/robot/commands/IntakeAssist.java
@@ -6,20 +6,33 @@ package frc.robot.commands;
 
 import edu.wpi.first.wpilibj2.command.Command;
 import frc.robot.subsystems.drivetrain.CommandSwerveDrivetrain;
+import frc.robot.subsystems.drivetrain.DriveConstants;
 import frc.robot.subsystems.intake.Intake;
 
 public class IntakeAssist extends Command {
-  public IntakeAssist(Intake intake, CommandSwerveDrivetrain drivetrain) {
-    // Use addRequirements() here to declare subsystem dependencies.
+  private final Intake intake;
+  private final CommandSwerveDrivetrain drivetrain;
+
+  public IntakeAssist(Intake m_intake, CommandSwerveDrivetrain m_drivetrain) {
+    this.intake = m_intake;
+    this.drivetrain = m_drivetrain;
+    addRequirements(m_intake, m_drivetrain);
   }
 
-  // Called when the command is initially scheduled.
   @Override
-  public void initialize() {}
+  public void initialize() {
+    intake.setPivotDown();
+    intake.setRollersIn();
+  }
 
   // Called every time the scheduler runs while the command is scheduled.
   @Override
-  public void execute() {}
+  public void execute() {
+    drivetrain.setControl(DriveConstants.drive
+      .withVelocityX(0.0)
+      
+    );
+  }
 
   // Called once the command ends or is interrupted.
   @Override
