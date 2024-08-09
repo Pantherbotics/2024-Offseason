7be91f5fb07d6f8be5d89cd49b20145666714805
soysauce2393 <hubblesoren@gmail.com>
2024-08-09 08:27:21 -0700

    tuned steer and drive gains for swerve

--- src/main/java/frc/robot/subsystems/drivetrain/TunerConstants.java
+++ src/main/java/frc/robot/subsystems/drivetrain/TunerConstants.java
@@ -21,13 +21,13 @@ public class TunerConstants {
     // The steer motor uses any SwerveModule.SteerRequestType control request with the
     // output type specified by SwerveModuleConstants.SteerMotorClosedLoopOutput
     private static final Slot0Configs steerGains = new Slot0Configs()
-        .withKP(100).withKI(0).withKD(0.2)
-        .withKS(0).withKV(1.5).withKA(0);
+        .withKP(50).withKI(0).withKD(0.2)
+        .withKS(0).withKV(0.675).withKA(0);
     // When using closed-loop control, the drive motor uses the control
     // output type specified by SwerveModuleConstants.DriveMotorClosedLoopOutput
     private static final Slot0Configs driveGains = new Slot0Configs()
-        .withKP(3).withKI(0).withKD(0)
-        .withKS(0).withKV(0).withKA(0);
+        .withKP(8.6).withKI(0).withKD(0)
+        .withKS(0.13413).withKV(0.11753).withKA(0.0087051);
 
     // The closed-loop output type to use for the steer motors;
     // This affects the PID/FF gains for the steer motors
@@ -41,6 +41,8 @@ public class TunerConstants {
     private static final double kSlipCurrentA = 150;
 
     private static final TalonFXConfiguration driveInitialConfigs = new TalonFXConfiguration();
+
+
         private static final TalonFXConfiguration steerInitialConfigs = new TalonFXConfiguration()
         .withCurrentLimits(
         new CurrentLimitsConfigs()
